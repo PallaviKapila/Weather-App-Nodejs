fbc7011d02f098ecdde385467a0b77d44ec7fa37
PallaviKapila <49369199+PallaviKapila@users.noreply.github.com>
2020-11-03 00:00:14 +0530

    Update forecast.js

--- web server/src/utils/forecast.js
+++ web server/src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+    const url = 'api.weatherstack.com/current?access_key=3ce24efb523d31ef441de308bd827fc4&query=' + latitude + ',' + longitude
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
@@ -9,7 +9,10 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. This high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, response.body.current.weather_descriptions[0] +
+          ". It is currently " +
+          response.body.current.temperature +
+          " degress out.")
         }
     })
 }
