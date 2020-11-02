23910e375771d3a3adb7a4396bc9591d68aadf07
PallaviKapila <49369199+PallaviKapila@users.noreply.github.com>
2020-11-02 18:14:31 +0530

    Update app.js

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,7 +4,8 @@ const url =
   "http://api.weatherstack.com/current?access_key=3ce24efb523d31ef441de308bd827fc4&query=37.8267,-122.4233&units=f";
 request({ url: url, json: true }, (error, response) => {
   console.log(
-    "It is currently " +
+    response.body.current.weather_descriptions[0] +
+      ". It is currently " +
       response.body.current.temperature +
       " degrees out. It feels like " +
       response.body.current.feelslike +
